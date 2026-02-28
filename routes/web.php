0880b5445928687da483f55b280e5c68ba393681
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2026-02-28 09:26:18 +0000

    update(filiere): modify crud component and routes
    - Update crud-table component for filiere
    - Adjust routes for new filiere views

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::resource('/groupes', FiliereController::class);
+    // Route::resource('/groupes', FiliereController::class);
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
@@ -20,7 +20,7 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::middleware(["auth","admin"])->group(function(){
-    Route::resource("groupes",FiliereController::class);
+    Route::resource("filieres",FiliereController::class);
 });
 
 
