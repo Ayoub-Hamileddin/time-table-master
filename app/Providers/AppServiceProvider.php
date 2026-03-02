e9841c777aeaf6e8981008fe508e8d9d598e76c1
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2026-03-02 01:05:03 +0000

    feat: update service provider bindings

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 use App\Models\Filiere;
+use App\Models\Groupe;
 use App\Policies\FilierePolicy;
+use App\Policies\GroupePolicy;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
@@ -23,5 +25,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Gate::policy(Filiere::class,FilierePolicy::class );
+        Gate::policy(Groupe::class,GroupePolicy::class );
     }
 }
