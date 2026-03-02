8f2d92ac6964e36e8a75c1a0021bd2588aa4a00c
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2026-03-02 01:05:18 +0000

    feat: update navigation menu with new items

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,6 +16,11 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('filieres.index')" :active="request()->routeIs('filieres.index')">
+                        {{ __('Filieres') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
