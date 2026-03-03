004c55f14f1e696f8685895bb8603149727110bc
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2026-03-03 01:19:09 +0000

    Update GroupeService with business logic enhancements

--- app/Services/GroupeService.php
+++ app/Services/GroupeService.php
@@ -2,13 +2,29 @@
 
 namespace App\Services;
 
+use App\Repositories\Groupe\GroupeInterfaceImpl;
+
 class GroupeService
 {
     /**
      * Create a new class instance.
      */
-    public function __construct()
+    public function __construct(
+        private GroupeInterfaceImpl $repository 
+    )
     {
         //
     }
+    public function createGroupe($data){
+        return $this->repository->create($data);
+    }
+    public function getGroupeById(int $id){
+        return $this->repository->getGroupeById($id);
+    }
+
+    public function updateGroupe($id,$data){
+        return $this->repository->update($id,$data);
+    }
+
+
 }
