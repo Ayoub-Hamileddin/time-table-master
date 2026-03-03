0963b0e94505b525a18d25528dbce5df063efcd3
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2026-03-03 01:19:07 +0000

    Update Groupe model with new relationships and attributes

--- app/Models/Groupe.php
+++ app/Models/Groupe.php
@@ -6,6 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Groupe extends Model
 {
+
+    protected $fillable = ["code","annee","filiere_id"];
+
     public function filiere(){
         return $this->belongsTo(Filiere::class);
     }
