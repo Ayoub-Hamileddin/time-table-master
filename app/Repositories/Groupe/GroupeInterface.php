62722e96dc16891028e0e490d6aa62fbb229d75b
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2026-03-03 01:19:14 +0000

    Add Groupe repository for data access layer

---
+++ app/Repositories/Groupe/GroupeInterface.php
@@ -0,0 +1,11 @@
+<?php 
+namespace App\Repositories\Groupe;
+
+use App\Models\Filiere;
+
+interface GroupeInterface{
+    public function create($data);
+    public function getGroupeById(int $id);
+
+    public function update(int $id,$data );
+}
---
+++ app/Repositories/Groupe/GroupeInterfaceImpl.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Repositories\Groupe;
+
+use App\Models\Groupe;
+use App\Repositories\Groupe\GroupeInterface;
+
+class GroupeInterfaceImpl implements GroupeInterface {
+
+
+
+    public function create($data)
+    {
+        return Groupe::create($data);
+    }
+    public function getGroupeById($id)
+    {
+        return Groupe::with("filiere")
+            ->findOrFail($id);
+    }
+
+    public function update(int $id ,$data ){
+        $groupe = $this->getGroupeById($id);
+        $groupe->update([
+            "code" => $data["code"],
+            "annee" => $data["annee"],
+            "filiere_id " => $data["filiere_id "],
+        ]);
+        return $groupe ;
+    }
+
+}
