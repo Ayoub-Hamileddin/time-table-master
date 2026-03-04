1eb3f115748f1d0a2542cb7e786a339cde8eed35
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2026-03-04 10:50:10 +0000

    Update GroupeInterface with new method signatures

--- app/Repositories/Groupe/GroupeInterface.php
+++ app/Repositories/Groupe/GroupeInterface.php
@@ -6,6 +6,6 @@ use App\Models\Filiere;
 interface GroupeInterface{
     public function create($data);
     public function getGroupeById(int $id);
-
     public function update(int $id,$data );
+    public function delete($filiere);
 }
