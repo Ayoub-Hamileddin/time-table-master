3f0b0abc3b95a3dbb3513918d7a127861c2fafda
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2026-03-04 17:31:02 +0000

    Add FormateurService with business logic

---
+++ app/Services/FormateurService.php
@@ -0,0 +1,26 @@
+<?php 
+namespace   App\Services;
+
+use App\Repositories\Formateur\FormateurInterfaceImpl;
+
+class FormateurService {
+
+    public function __construct(
+        private FormateurInterfaceImpl $repository 
+    )
+    {
+        
+    }
+
+
+    public function getAllFormateur(){
+        return $this->repository->findAll();
+    }
+    public function createFormateur($data){
+        return $this->repository->create($data);
+    }
+
+    public function updateFormateur($data,$formateur){
+        return $this->repository->update($data,$formateur);
+    }
+}
