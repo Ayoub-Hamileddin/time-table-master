1fc5ab2087cb0884f3812861181a42825e26eb83
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2026-03-04 17:30:50 +0000

    Update navigation with formateur management link

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -26,6 +26,11 @@
                         {{ __('Groupes') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('formateurs.index')" :active="request()->routeIs('formateurs.index')">
+                        {{ __('Formateurs') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
