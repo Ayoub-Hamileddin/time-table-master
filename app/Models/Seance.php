5bb3e0a218594e1c0b7df2e50461d2c6b0fdf409
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2026-03-11 23:18:13 +0000

    Update Seance model with new fields/relationships

--- app/Models/Seance.php
+++ app/Models/Seance.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Seance extends Model
 {
+
+
+    protected $fillable = ["groupe_id","formateur_id","salle_id","jour","creneau"];
+
     public function groupe(){
         return $this->belongsTo(Groupe::class);
     }
