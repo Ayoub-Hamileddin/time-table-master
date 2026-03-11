b4cdf2cf617c416be3f5d8868b144ecf8d132b8c
ayoub hamileddin <ayoub.hamileddine@gmail.com>
2026-03-11 23:18:17 +0000

    Update web routes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Admin\FiliereController;
 use App\Http\Controllers\Admin\FormateurController;
 use App\Http\Controllers\Admin\GroupeController;
 use App\Http\Controllers\Admin\SalleController;
+use App\Http\Controllers\Admin\SeanceController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,19 +15,23 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    // Route::resource('/groupes', FiliereController::class);
-
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 Route::middleware(["auth","admin"])->group(function(){
+    // ------------------    Filiere Routes  ---------------
     Route::resource("filieres",FiliereController::class);
     Route::get("/filieres/option/{annee}",[FiliereController::class,"getOptionAnnee"]);
+    // ------------------    Groupe Routes  ---------------
     Route::resource("groupes",GroupeController::class);
+    // ------------------    Formateur Routes  ---------------
     Route::resource("formateurs",FormateurController::class);
+    // ------------------    Salle Routes  ---------------
     Route::resource("salles",SalleController::class);
+    // ------------------    Seance Routes  ---------------
+    Route::resource("seances",SeanceController::class);
 });
 
 
